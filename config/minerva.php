b3b562f4347d1e96d63ac176f062cb7177a3de4b
Dragon <dragonofmercy@gmail.com>
2025-01-16 12:31:53 +0100

    Default minerva config

---
+++ config/minerva.php
@@ -0,0 +1,26 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Exchange rate configuration
+    |--------------------------------------------------------------------------
+    |
+    | These configuration options determine the driver used to determine and
+    | manage the source of the exchange rates informations.
+    |
+    | Supported drivers: "cached", "frankfurter", "xrates"
+    |
+    */
+
+    'rates' => [
+        'driver' => env('RATES_CACHE_STORE', 'cached'),
+        'cache' => [
+            'store' => env('RATES_CACHE_STORE'),
+            'driver' => env('RATES_CACHE_DRIVER', 'frankfurter'),
+            'key' => env('RATES_CACHE_KEY', 'cached_exchange_rates'),
+            'ttl' => env('RATES_CACHE_TTL', 900),
+        ]
+    ]
+
+];
