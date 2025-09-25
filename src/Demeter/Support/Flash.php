07e75ed99f2701cda46019c1f65d2f1670eb2d31
Dragon <dragonofmercy@gmail.com>
2025-09-25 10:29:24 +0200

    Fix parameter name

--- src/Demeter/Support/Flash.php
+++ src/Demeter/Support/Flash.php
@@ -12,16 +12,16 @@ class Flash
      * @param string $name The name or key under which the flash message will be stored.
      * @param string $message The content of the flash message.
      * @param string $type The type of the flash message (e.g., success, error, etc.). Defaults to FlashComponent::TYPE_SUCCESS.
-     * @param bool $presistant Determines whether the flash message should persist across multiple requests. Defaults to true.
+     * @param bool $persistant Determines whether the flash message should persist across multiple requests. Defaults to true.
      * @throws InvalidArgumentException If an invalid type is provided that is not in FlashComponent::VALID_TYPES.
      */
-    public static function registerFlash(string $name, string $message, string $type = FlashComponent::TYPE_SUCCESS, bool $presistant = true): void
+    public static function registerFlash(string $name, string $message, string $type = FlashComponent::TYPE_SUCCESS, bool $persistant = true): void
     {
         if(!in_array($type, FlashComponent::VALID_TYPES)){
             throw new InvalidArgumentException('Type "' . $type . '" is not valid. Valid types are: ' . implode(', ', FlashComponent::VALID_TYPES));
         }
-        
-        $method = $presistant ? 'flash' : 'now';
+
+        $method = $persistant ? 'flash' : 'now';
         session()->$method($name, ['message' => $message, 'type' => $type]);
     }
 }
