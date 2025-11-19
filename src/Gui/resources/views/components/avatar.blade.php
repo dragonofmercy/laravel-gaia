24cc00b261751e5a8f3d965f64e8af15d5dcf804
Dragon <dragonofmercy@gmail.com>
2025-11-19 07:50:16 +0100

    Switch div to span

--- src/Gui/resources/views/components/avatar.blade.php
+++ src/Gui/resources/views/components/avatar.blade.php
@@ -1,7 +1,7 @@
-<div {{ $attributes->merge(['class' => 'avatar', 'style' => $image]) }}>
+<span {{ $attributes->merge(['class' => 'avatar', 'style' => $image]) }}>
     @if(strlen($slot))
         {{ $slot }}
     @elseif(!$image)
         {{ $content }}
     @endif
-</div>
+</span>
