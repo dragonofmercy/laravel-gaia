0e73ca98218a97e0eaf8d243bd2fe1d78bdc2952
Dragon <dragonofmercy@gmail.com>
2026-03-05 08:41:39 +0100

    Change tabler icon system

--- src/Gui/View/Components/TablerIcon.php
+++ src/Gui/View/Components/TablerIcon.php
@@ -17,12 +17,7 @@ class TablerIcon extends Component
 
     public function render(): string
     {
-        $cache = static::getCache();
-        $type = $this->filled ? 'filled' : 'outline';
-        $svg = $cache[$this->name]['svg'][$type] ?? $cache['exclamation-circle']['svg'][$type];
-        $class = Str::join('icon', $this->class);
-
-        return Str::replace('class="icon"', 'class="' . $class . '"', $svg);
+        return static::renderIcon($this->name, $this->filled ? 'filled' : 'outline', $this->class);
     }
 
     /**
@@ -32,9 +27,51 @@ class TablerIcon extends Component
      */
     protected static function reloadCache(): void
     {
-        $jsonPath = __DIR__ . '/../../resources/icons/tabler.json';
-        $jsonContent = file_get_contents($jsonPath);
-        self::$cache = json_decode($jsonContent, true);
+        $jsonPath = __DIR__ . '/../../resources/icons/';
+        self::$cache = [
+            'outline' => json_decode(file_get_contents($jsonPath . 'tabler-nodes-outline.json'), true),
+            'filled' => json_decode(file_get_contents($jsonPath . 'tabler-nodes-filled.json'), true)
+        ];
+    }
+
+    /**
+     * Renders an SVG icon based on given parameters.
+     *
+     * @param string $name The name of the icon to render.
+     * @param string $type The type of the icon (e.g., 'outline' or 'filled'). Defaults to 'outline'.
+     * @param string $class Additional CSS classes to apply to the icon. Defaults to an empty string.
+     * @return string The generated SVG icon as a string.
+     */
+    public static function renderIcon(string $name, string $type = 'outline', string $class = ""): string
+    {
+        $cache = static::getCache();
+
+        if(!array_key_exists($name, $cache[$type])){
+            $name = 'exclamation-circle';
+        }
+
+        $iconContent = "";
+
+        foreach($cache[$type][$name] as $part){
+            $iconContent.= tag($part[0], $part[1]);
+        }
+
+        $attr = [
+            'xmlns' => 'http://www.w3.org/2000/svg',
+            'width' => '24',
+            'height' => '24',
+            'viewBox' => '0 0 24 24',
+            'fill' => $type === 'filled' ? 'currentColor' : 'none',
+            'stroke-linecap' => 'round',
+            'stroke-linejoin' => 'round',
+            'class' => Str::join('icon', $class)
+        ];
+
+        if($type === 'outline'){
+            $attr['stroke'] = 'currentColor';
+        }
+
+        return content_tag('svg', $iconContent, $attr);
     }
 
     /**
